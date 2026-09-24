93604076db9e284ccb51f20632e2d15acb70988d
THorn-13 <yuu10k@gmail.com>
2026-09-25 01:22:31 +0900

    Add files via upload

---
+++ qmk_firmware/keyboards/thr218_v3/keymaps/vial/config.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#define EE_HANDS
+#define VIAL_KEYBOARD_UID {0xc2, 0xda, 0x87, 0x82, 0x02, 0xc9, 0xbb, 0x14}
+#define VIAL_TAP_DANCE_ENTRIES 8
+#define VIAL_COMBO_ENTRIES 8
+#define PICO_FLASH_SIZE_BYTES (1 * 1024 * 1024)
